02bb38f5a8fc6840d7674b1b33122687414748d3
dayln <dayln@limesand.dev>
2026-02-22 17:08:14 -0600

    Setup shadcn-svelte for ui components

--- front-end/src/routes/+page.svelte
+++ front-end/src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
 	const ipcHandle = (): void => window.electron.ipcRenderer.send('ping');
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<button onclick={ipcHandle}>Send IPC Ping</button>
+<Button onclick={ipcHandle}>Send IPC Ping</Button>
