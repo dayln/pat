25340943c60c32bd462db82be975b50a34e786e1
dayln <dayln@limesand.dev>
2026-02-22 17:17:59 -0600

    Move electron dir into src share modules easier

--- front-end/electron.vite.config.ts
+++ front-end/electron.vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
 		build: {
 			rollupOptions: {
 				input: {
-					index: resolve(__dirname, 'electron/main/index.ts')
+					index: resolve(__dirname, 'src/electron/main/index.ts')
 				}
 			}
 		}
@@ -17,7 +17,7 @@ export default defineConfig({
 		build: {
 			rollupOptions: {
 				input: {
-					index: resolve(__dirname, 'electron/preload/index.ts')
+					index: resolve(__dirname, 'src/electron/preload/index.ts')
 				}
 			}
 		}
--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
 						instances: [{ browser: 'chromium', headless: true }]
 					},
 					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
-					exclude: ['src/lib/server/**']
+					exclude: ['src/lib/server/**', 'src/electron/**/*']
 				}
 			}
 		]
