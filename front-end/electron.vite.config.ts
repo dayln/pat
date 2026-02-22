e9f3fa5cb4eeede9d2e2bef380d28dbf7a17b526
dayln <dayln@limesand.dev>
2026-02-22 17:05:25 -0600

    Initial SvelteKit and electron scaffolding

---
+++ front-end/electron.vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'electron-vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+	main: {
+		build: {
+			rollupOptions: {
+				input: {
+					index: resolve(__dirname, 'electron/main/index.ts')
+				}
+			}
+		}
+	},
+	preload: {
+		build: {
+			rollupOptions: {
+				input: {
+					index: resolve(__dirname, 'electron/preload/index.ts')
+				}
+			}
+		}
+	},
+	renderer: {
+		plugins: [tailwindcss(), sveltekit()],
+		build: {
+			rollupOptions: {
+				input: {
+					index: resolve(__dirname, 'src/app.html')
+				}
+			}
+		}
+	}
+});
---
+++ front-end/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	const ipcHandle = (): void => window.electron.ipcRenderer.send('ping');
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<button onclick={ipcHandle}>Send IPC Ping</button>
