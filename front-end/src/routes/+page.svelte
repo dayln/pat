25127a07cdf98759fdb29df8a5d9d6d8bada1a03
dayln <dayln@limesand.dev>
2026-02-23 19:46:11 -0600

    Changed to Skeleton for more native element usage

--- front-end/src/routes/+page.svelte
+++ front-end/src/routes/+page.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-	import Button from '$lib/components/ui/button/button.svelte';
 	const ipcHandle = (): void => window.electron.ipcRenderer.send('ping');
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<Button onclick={ipcHandle}>Send IPC Ping</Button>
+<button type="button" class="btn preset-filled" onclick={ipcHandle}>Send IPC Ping</button>
